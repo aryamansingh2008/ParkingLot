d75293d2b422e0cec0134c5e23ad18da06c1a77f
Aryaman <aryaman@gameberrylabs.com>
2024-08-21 17:01:52 +0530

    Add vehicle factory

---
+++ src/vehicle/factory/vehicleFactory.go
@@ -0,0 +1,21 @@
+package factory
+
+import (
+	commonTypes "github.com/aryamansingh2008/ParkingLot/src/common/types"
+	"github.com/aryamansingh2008/ParkingLot/src/vehicle"
+	vehicleTypes "github.com/aryamansingh2008/ParkingLot/src/vehicle/constants/vehicleType"
+	"github.com/aryamansingh2008/ParkingLot/src/vehicle/errors"
+	types "github.com/aryamansingh2008/ParkingLot/src/vehicle/types"
+)
+
+type VehicleFactory struct{}
+
+func (f *VehicleFactory) CreateVehicle(vehicleType types.VehicleType, registrationNo string,
+	color commonTypes.Color) (vehicle.IVehicle, error) {
+	switch vehicleType {
+	case vehicleTypes.Car:
+		return vehicle.NewCar(registrationNo, color)
+	default:
+		return nil, errors.InvalidType
+	}
+}
